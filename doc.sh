915e88021696459732f3db8d110d3023696016f7
Hans Höglund <hh_glund@hotmail.com>
2010-11-12 01:38:02 +0100

    Major changes 3, added tex template

--- doc.sh
+++ doc.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 pandoc manual.md -o manual.html --toc -c styles.css --template template.html;
-#markdown2pdf manual.md;
 
 osascript -e 'tell application "Firefox" to activate';
---
+++ pdf.sh
@@ -0,0 +1,4 @@
+markdown2pdf manual.md --template template.tex;
+ 
+open manual.pdf;
+#osascript -e 'tell application "Preview" to activate';
