3254bf7b528eef786756846e1121112da512acb9
Hans Höglund <hh_glund@hotmail.com>
2010-12-06 03:25:14 +0100

    Added ref

---
+++ src/java/vitry/primitive/Reference.java
@@ -0,0 +1,12 @@
+package vitry.primitive;
+
+/**
+ * Invariants:
+ *  this.references(o) -> this.equals(o)
+ *  
+ * @author hans
+ */
+public interface Reference {
+    
+    public boolean references(Value v);
+}
