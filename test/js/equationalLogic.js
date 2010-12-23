2756f8315cf1cf4a16c1955c5d7fded0a1a1fc39
Hans Höglund <hh_glund@hotmail.com>
2010-12-23 02:46:36 +0100

    Finished working version of eq logic test

--- test/js/equationalLogic.js
+++ test/js/equationalLogic.js
@@ -4,8 +4,6 @@
  * Test of the basic equational logic
  * See TODO
  */
-OPTMIZATION = true;
-
 Term = function() {};
 
 Atom = function() {};
@@ -19,15 +17,22 @@ Product = function(/* ... */){
     this[i] = arguments[i];
 };
 
-Union = function(/* ... */){
-  for(let i=0; i<arguments.length;i++)
-    this[i] = arguments[i];
-};
-
 Set = function(/* ... */){
   for(let i=0; i<arguments.length;i++)
     this[i] = arguments[i];
 };    
+
+Union = function(/* ... */){
+  if (arguments.length <= 1) throw Error("A union requires at least two components.");
+  for(let i=0; i<arguments.length;i++)
+    this[i] = arguments[i];
+};
+
+Intersection = function(/* ... */){
+  if (arguments.length <= 1) throw Error("An intersection requires at least two components.");
+  for(let i=0; i<arguments.length;i++)
+    this[i] = arguments[i];
+};
          
 Term.prototype = {
   equals : function(v) {
@@ -41,115 +46,77 @@ Term.prototype = {
 
 Atom.prototype = new Term();
 Product.prototype = new Term();
-Union.prototype = new Term();
 Set.prototype = new Term();
+Union.prototype = new Term();
+Intersection.prototype = new Term();
 Value.prototype = new Atom();
+
+
 
 
 // Implementations
 
-/*
- * x:Value = y:Value 
- *     (trivial)
- */
+
+
 Value.prototype.equals = function(that) {  
+  // trivial
   return  this.wrap === that.wrap;
 }
 
-/*
- * x:Product = y:Product
- *     iff x = () and y = ()
- *     or x and y have the same elements in the same order
- */
 Product.prototype.equals = function(that) {
   if (!(that instanceof Product)) return false;
 
-//  if (OPTMIZATION) {
-    if (this.keys().length !== that.keys().length) 
-      return false;                               
-//  }
-  
+  if (this.keys().length !== that.keys().length) 
+    return false;                               
+
+  // x = () and y = ()
   if (this.keys().length == 0)
     return true;
+  
+  // head(x) = head(y) and tail(x) = tail(y)
   for (i = 0; i < this.keys().length; i++) {
     if (!this[i].equals(that[i])) return false;    
   }
   return true;
 }
 
-/*
- * x:Union = y:Union
- *     iff for each element e1 in x, for some element e2 of y
- *         e1 matches e2 or e2 matches e1
- */
-Union.prototype.equals = function(that) {
-  if (!(that instanceof Union)) return false;
-                    
-  if (this.keys().length == 0 && that.keys().length == 0)
-    return true;
-  
-  // iterate members
-  for (i = 0; i < this.keys().length; i++) {
-    let found = false;  
-    // perform linear search
-    for (j = 0; j < that.keys().length; j++) {
-      if(this[i].matches(that[j]) || that[j].matches(this[i])) 
-        found = true;
-    }
-    if (!found) return false;
-  }                 
-  return true;
-}
-
-/*
- * x:Set = y:Set
- *     iff for each element e1 in x, for some element e2 of y, e1 equals e2
- *         and for each element e1 in y, for some element e2 of x, e1 equals e2
- */
 Set.prototype.equals = function(that) {
   if (!(that instanceof Set)) return false;
-  
-  if (this.keys().length == 0 && that.keys().length == 0)
-    return true;
-            
-  // iterate members
-  for (i = 0; i < this.keys().length; i++) {
-    let found = false;  
-    // perform linear search
-    for (j = 0; j < that.keys().length; j++) {
-      if(this[i].equals(that[j])) found = true;
-    }
-    if (!found) return false;
-  }                 
-  // iterate members of that
-  for (i = 0; i < that.keys().length; i++) {
-    let found = false;
-    for (j = 0; j < this.keys().length; j++) {
-      if(that[i].equals(this[j])) found = true;
-    }
-    if (!found) return false;
-  }
-  return true;
+
+  // x = {} and y = {}
+//  if (this.keys().length === 0 && that.keys().length === 0) return true;
+//  if (this.keys().length === 0 || that.keys().length === 0) return false;
+
+  return this.matches(that) && that.matches(this);
 }
 
-/*
- * x:Value :: y:Value
- *     iff x = y
- */
+Union.prototype.equals = function(that) {
+  return this.matches(that) && that.matches(this);
+}
+
+Intersection.prototype.equals = function(that) {
+  return this.matches(that) && that.matches(this);
+}
+
+
+
+
+
 Value.prototype.matches = function(that) {
-  if (that instanceof Atom) 
+  if (that instanceof Atom) {
+    // x = y
     return this.equals(that);
+  }
   return false;
 }
 
-/*
- * x:Product :: y:Product
- *     iff x = y
- *     or elements of x mathes y ... TODO
- */
 Product.prototype.matches = function(that) {
   if (!(that instanceof Product)) return false;
+  
+  // x = y
   if (this.equals(that)) return true;
+
+  // head(x) : head(y) and tail(x) : tail(y)
 
   if (this.keys().length !== that.keys().length) 
     return false;                            
@@ -159,63 +126,111 @@ Product.prototype.matches = function(that) {
   return true;
 }
 
-/*
- * x:Atom|Product :: y:Union
- *     TODO
- * x:Union :: y:Union
- *     TODO
- * x:Set :: y:Union
- *     TODO
- */
-Union.prototype.matches = function(that) {
-  if (that instanceof Atom || that instanceof Product) {
-    for each (v in this)
-      if (v.matches(that)) return true;
-  }
-  if (that instanceof Union) {
-    for each (v in this)
-      if (v.matches(that)) return true;
-  }
-  if (that instanceof Set) {
-    for each (v in this)
-      if (v.equals(that)) return true;
-  }
-  
-  return false;
-}
-
-/*
- * x:Atom|Product :: y:Set
- *     TODO
- * x:Union :: y:Set
- *     TODO
- * x:Set :: y:Set
- *     TODO
- */
 Set.prototype.matches = function(that) {
   if (that instanceof Atom || that instanceof Product) {
+    // iff x = head(y) or x = tail(y)
     for each (v in this)
       if (v.equals(that)) return true;
   }
+  if (that instanceof Set) {
+    // for each element E in x, E = head(y) or E = tail(y)
+    for each (thatVal in that) {
+      let some = false;
+      for each (thisVal in this) {
+        if (thisVal.equals(thatVal)) some = true;
+      }
+      if (!some) return false;    
+    }
+    return true;
+  }
   if (that instanceof Union) {
+    throw new Error("No implementation.");
+  }
+  if (that instanceof Intersection) {
+    throw new Error("No implementation.");
+  }
+  return false;
+}
+
+Union.prototype.matches = function(that) {
+  if (that instanceof Atom || that instanceof Product) {
+    // iff x : head(y) or x : tail(y)
     for each (v in this)
       if (v.matches(that)) return true;
   }
   if (that instanceof Set) {
-    for each (v in this)
-      if (v.equals(that)) return true;
+    // for each element E in x, E : head(y) or E : tail(y)
+    for each (thatVal in that) {
+      let some = false;
+      for each (thisVal in this) {
+        if (thisVal.matches(thatVal)) some = true;
+      }
+      if (!some) return false;    
+    }
+    return true;
   }
-  
+  if (that instanceof Union) {
+    // for each element E in x, E : head(y) or head(y) : E or E : tail(y) or tail(y) : E
+    for each (thatVal in that) {
+      let some = false;
+      for each (thisVal in this) {
+        if (thisVal.matches(thatVal)) some = true;
+        if (thatVal.matches(thisVal)) some = true;
+      }
+      if (!some) return false;    
+    }
+    return true;
+  }
+  if (that instanceof Intersection) {
+    throw new Error("No implementation.");
+  }
   return false;
 }
+
+Intersection.prototype.matches = function(that) {
+  if (that instanceof Atom || that instanceof Product) {
+    // iff x : head(y) and x : tail(y)
+    for each (v in this)
+      if (!v.matches(that)) return false;
+    return true;
+  }
+  if (that instanceof Set) {
+    // for each element E in x, E : head(y) and E : tail(y)
+    for each (thatVal in that) {
+      let every = true;
+      for each (thisVal in this) {
+        if (!thisVal.matches(thatVal)) every = false;
+      }
+      if (!every) return false;    
+    }
+    return true;
+  }
+  if (that instanceof Union) {
+    throw new Error("No implementation.");
+  }
+  if (that instanceof Intersection) {
+    // for each element E in x, ( E : head(y) or head(y) : E ) and ( E : tail(y) or tail(y) : E )
+    for each (thatVal in that) {
+      let every = true;
+      for each (thisVal in this) {
+        // FIXME
+        if (!(thisVal.matches(thatVal)) && !(thatVal.matches(thisVal))) every = false;
+//        if (!(thisVal.matches(thatVal)) && thatVal.matches(thisVal)) every = false;
+//        if (thisVal.matches(thatVal) && !(thatVal.matches(thisVal))) every = false;
+      }
+      if (!every) return false;    
+    }
+    return true;
+  }
+  return false;
+}
+
     
 
-/*
- * ? = ?
- * for all x, x :: ?
- * 
- * FIXME can not match wildcards in sets
- */
+
+
+// Wildcard
+
 wildcard = new Value();
 
 wildcard.equals = function(other) {
@@ -232,6 +247,7 @@ wildcard.toString = function() {
 
 
 
+// Testing
 
 
 Product.prototype.toString = function() {
@@ -258,6 +274,18 @@ Union.prototype.toString = function() {
   return str;
 }
 
+Intersection.prototype.toString = function() {
+  var str = "";
+  for (k in this) {
+    if (k > 0) {
+      str += " & ";
+    }
+    str += this[k];
+  }
+  str += "";
+  return str;
+}
+
 
 Set.prototype.toString = function() {
   var str = "{";
@@ -281,6 +309,7 @@ Object.enumerable(Term.prototype,    ["toString", "equals", "matches"], false);
 Object.enumerable(Atom.prototype,    ["toString", "equals", "matches"], false);
 Object.enumerable(Product.prototype, ["toString", "equals", "matches"], false);
 Object.enumerable(Union.prototype,   ["toString", "equals", "matches"], false);
+Object.enumerable(Intersection.prototype, ["toString", "equals", "matches"], false);
 Object.enumerable(Set.prototype,     ["toString", "equals", "matches"], false);
 Object.enumerable(Value.prototype,   ["toString", "equals", "matches"], false);
 
@@ -317,6 +346,7 @@ pads = [];
 
 P = Product;
 U = Union;
+I = Intersection;
 S = Set;
 V = Value;
 a = new V("a");
@@ -329,56 +359,31 @@ d = new V("d");
 
 // Cases
 
-//printEquals(a, a);
-//printEquals(a, b);
-//print();
-//
-//printEquals(new P(a,b,c), new P(a,b,c));
-//printEquals(new P(b,a,c), new P(a,b,c));
-//printEquals(new U(a,b,c), new U(a,b,c));
-//printEquals(new U(b,a,c), new U(a,b,c));
-//printEquals(new S(a,b,c), new S(a,b,c));
-//printEquals(new S(b,a,c), new S(a,b,c));
-//print();
-//
-//
-//printMatches(a, a);
-//printMatches(a, b);
-//printMatches(a, new P(a, b));
-//printMatches(new P(a, b), new P(a, b));
-//print();
-//
-//printMatches(a, new U(a,b,c));
-//printMatches(new P(a, b), new U(a,b, new P(a, b)));
-//print();
-//
-//printMatches();
-
-
 printEquals(a, a);
+printEquals(new P, new P);
 printEquals(new P(a, b), new P(a, b));
 printEquals(new P(a, new P(b, c)), new P(a, new P(b, c)));
 printEquals(new U(a, b), new U(a, b));
 printEquals(new U(a, new U(b, c)), new U(new U(a, b), c));
-printEquals(new S(a, b, c), new S(a, b, c));
+printEquals(new S(a, b), new S(b, a));
 printEquals(new S,new S);
 printEquals(new S(new S),new S(new S));
-//(()) = () no unary tuples
 
 print();
+printMatches(a, a);
+printMatches(new P, new P);
 printMatches(new P(a, b), new P(a, b));
 printMatches(a, new U(a, b));
 printMatches(b, new U(a, b));
+printMatches(a, new I(a, a));
+printMatches(a, new I(a, new U(a, b)));
 printMatches(c, new U(a, new U(b, c)));
 printMatches(c, new U(a, wildcard));
 printMatches(a, new S(a, b));
 printMatches(b, new S(a, b));
-//printMatches(c, new S(a, wildcard));
 printMatches(a, new S(a, new S(b, c)));
-printMatches(new S(b, c), new S(a, new S(b, c)));
 printEquals(new S, new S);
 printEquals(new S(new S), new S(new S));
-// printEquals(new U(new U), new U(new U(new U))); no empty unions
 printMatches(a, new U(a,new P(b,new S(c,d))))
 printMatches(new P(b, d), new U(a,new P(b,new S(c,d))))
 printMatches(new P(a, b, c), new P(a,new S(a,b), new U(b,new U(c,d))))
@@ -387,16 +392,27 @@ printEquals(wildcard, wildcard);
 printMatches(a, wildcard);
 printMatches(new P(a, b), new P(wildcard, wildcard));
 printMatches(new P(a, new P(a, a)), new P(wildcard, new P(wildcard, wildcard)));
+printMatches(new U(a,b), new U(a,b));
+printMatches(new S(a,b), new S(a,b));
+printMatches(new U(a,b), new U(a,new U(b,c)));
+printMatches(new U(a,b), new U(a,new U(b,c)));
+printMatches(a, new I(a,new U(a,b)));
+printMatches(new I(a,a), new I(a,new U(a,b)));
+printMatches(new I(a,b), new I(a,new U(a,b)));
 
 print();
 print("false cases:");                    
+
+printMatches(a, b);
+printMatches(a, new P(a, b));
 printEquals(new P(a, b), new P(b, a));
 printEquals(new S, new S(new S));
 printEquals(new S(a, new S(b, c)), new S(new S(a, b), c));
-printMatches(new U(a,b), new U(a,b));
-printMatches(new S(a,b), new S(a,b));
-printMatches(new U(a,b), new S(a,b));
+printMatches(a, new I(a, b));
+printMatches(new S(b, c), new S(a, new S(b, c)));
 printMatches(new S(a, b, c), new S(a, new S(b, c)));
 printMatches(new P(a, b, c), new P(a,new S(a,b), new S(b,new U(c,d))))
+printMatches(new I(a,b), new I(a,new U(a,b), c));
+//printMatches(new U(a,b), new S(a,b));
 
 
