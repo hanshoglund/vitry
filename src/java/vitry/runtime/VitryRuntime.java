e35ea84cd37b9fb35b115ca18ee133c7db01cab3
Hans Höglund <hh_glund@hotmail.com>
2011-04-05 03:45:47 +0200

    Added (.'') and ($). Not sure about the latter.

--- src/java/vitry/runtime/VitryRuntime.java
+++ src/java/vitry/runtime/VitryRuntime.java
@@ -278,6 +278,7 @@ public final class VitryRuntime
         // Fixities
 
         prelude.defFix("(..')",    12, false, false);
+        prelude.defFix("(.'')",    12, false, false);
         prelude.defFix("(.')",     12, false, false);
         prelude.defFix("(..)",     12, false, false);
         prelude.defFix("(.)",      12, false, false);
